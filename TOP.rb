928005a8a2cb72e4fdae1b67dd53491d872222be
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2017-11-22 01:03:00 -0300

    Add files via upload

--- TOP.rb
+++ TOP.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
-require 'optparse'
+
 require 'socket'
 require 'timeout'
 
-ips = ARGV[0]
+iph = ARGV[0]
 sum = 0
 hilos = []
 
@@ -13,12 +13,12 @@ puts ""
 for i in 1..1024 do
 	hilos << Thread.new(i) do |j|
 		begin
-			Timeout::timeout(3){TCPSocket.new(ips, j)}
-		rescue => ex
-			
+			Timeout::timeout(5){TCPSocket.new(iph, j)}
+		rescue
+			next
 		else
-		puts "[+] Host #{ips} | Open port #{j}"
-		sum+= 1
+			print "[+] Host #{iph} | Open port #{j}\n"
+			sum+= 1
 		end
 	end
 end
@@ -26,5 +26,6 @@ end
 hilos.each do |t|
 	t.join
 end
+
 puts ""
-puts "[!] Done! | Target: #{ips} | Total open ports: #{sum}."
+puts "[!] Done! | Target: #{iph} | Total open ports: #{sum}."
