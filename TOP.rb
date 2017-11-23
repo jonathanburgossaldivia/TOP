0fcce79ec9a1690f2b4760af7baaf077fad45a1d
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2017-11-23 00:05:24 -0300

    Update TOP.rb

--- TOP.rb
+++ TOP.rb
@@ -1,23 +1,55 @@
 #!/usr/bin/ruby
 
+require 'optparse'
 require 'socket'
 require 'timeout'
 
-iph = ARGV[0]
+options = {}
 sum = 0
 hilos = []
+
+OptionParser.new do |opts|
+	puts ""
+	opts.banner += " [arguments...]"
+	opts.separator ""
+	opts.version = "0.1"
+	opts.on('-t', '--target TARGET', 'Objective to scan, example: 192.168.0.1.') do |target|
+		options[:target] = target;
+	end
+	opts.on('-r', '--range RANGE', 'Range of ports, example: 80.') do |range|
+		options[:range] = range;
+	end
+	begin
+		opts.parse!
+	rescue OptionParser::ParseError => error
+		$stderr.puts error
+		$stderr.puts "[!] -h or --help to show valid options."
+		exit 1
+	end
+end
+
+if options[:target] == nil
+	$stderr.puts "[!] -h or --help to show valid options."
+	exit 1
+end
+
+range = options[:range].to_i
+
+if range.to_s.empty?
+	range  = 80
+end
 
 puts ""
 puts "Tool by Jonathan Burgos Saldivia >"
 puts ""
-for i in 1..1024 do
+for i in 1..range do
 	hilos << Thread.new(i) do |j|
 		begin
-			Timeout::timeout(5){TCPSocket.new(iph, j)}
+			Timeout::timeout(5){TCPSocket.new(options[:target], j)}
 		rescue
 			next
 		else
-			print "[+] Host #{iph} | Open port #{j}\n"
+			print "[+] Host #{options[:target]} | Open port #{j}\n"
 			sum+= 1
 		end
 	end
@@ -28,4 +60,4 @@ hilos.each do |t|
 end
 
 puts ""
-puts "[!] Done! | Target: #{iph} | Total open ports: #{sum}."
+puts "[!] Done! | Target: #{options[:target]} | Total open ports: #{sum}."
